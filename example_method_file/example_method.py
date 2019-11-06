caff61eb7a00ec284475d699601ef8129dd79a3a
Chi-Kuan Lin <26458897+chikuanlin@users.noreply.github.com>
2019-11-06 13:18:06 -0500

    add solver example

---
+++ example_method_file/example_method.py
@@ -0,0 +1,13 @@
+import os
+import sys
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PARENT_DIR = os.path.abspath(os.path.join(BASE_DIR, os.path.pardir))
+sys.path.append(PARENT_DIR)
+sys.path.append(BASE_DIR)
+from base_solver import BaseSolver
+from constants import *
+
+class ExampleSolver(BaseSolver):
+    
+    def __init__(self):
+        super(ExampleSolver, self).__init__()
