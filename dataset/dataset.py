727403b65b5c6ce97f2fcbcddb07e54d2046231c
Chi-Kuan Lin <chikuan95@gmail.com>
2019-11-13 19:53:23 -0500

    dataset refactored

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -6,7 +6,6 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import string
 from tqdm import tqdm 
-# import re 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PARENT_DIR = os.path.abspath(os.path.join(BASE_DIR, os.path.pardir))
@@ -29,16 +28,19 @@ class WhatsCookingDataset:
         self.ingredient2id = {}
         self.id2cuisine = []
         self.cuisine2id = {}
-        with open(file_path, encoding='utf-8', mode = 'r') as json_file:
-            for item in json.load(json_file):
-                self.cuisines.append(Cuisine(**item))
-                if item['cuisine'] not in self.cuisine2id:
-                    self.id2cuisine.append(item['cuisine'])
-                    self.cuisine2id[item['cuisine']] = len(self.id2cuisine)-1
-                for ingredient in item['ingredients']:
-                    if ingredient not in self.ingredient2id:
-                        self.id2ingredient.append(ingredient)
-                        self.ingredient2id[ingredient] = len(self.id2ingredient)-1
+        if file_path is not None:
+            print("Loading What's Cooking training dataset ...")
+            with open(file_path, encoding='utf-8', mode = 'r') as json_file:
+                for item in tqdm(json.load(json_file)):
+                    self.cuisines.append(Cuisine(**item))
+                    if item['cuisine'] not in self.cuisine2id:
+                        self.id2cuisine.append(item['cuisine'])
+                        self.cuisine2id[item['cuisine']] = len(self.id2cuisine)-1
+                    for ingredient in item['ingredients']:
+                        if ingredient not in self.ingredient2id:
+                            self.id2ingredient.append(ingredient)
+                            self.ingredient2id[ingredient] = len(self.id2ingredient)-1
+            print("Successfully loaded What's Cooking training dataset!")
 
     def __len__(self):
         return len(self.cuisines)
@@ -64,21 +66,19 @@ class WhatsCookingDataset:
                 f.write(ingredient + '\n')
 
     def load_test_file(self, file_path='dataset/test.json'):
+        print("Loading What's Cooking testing dataset ...")
         cuisines = []
         with open(file_path, encoding='utf-8', mode = 'r') as json_file:
-            for item in json.load(json_file):
+            for item in tqdm(json.load(json_file)):
                 cuisines.append(Cuisine(**item))
+        print("Successfully loaded What's Cooking testing dataset!")
         return cuisines
     
     
-class WhatsCookingStemmedDataset:
+class WhatsCookingStemmedDataset(WhatsCookingDataset):
     def __init__(self, file_path='dataset/train.json'):
-        print("Loading and stemming What's Cooking dataset ...")
-        self.cuisines = []
-        self.id2ingredient = []
-        self.ingredient2id = {}
-        self.id2cuisine = []
-        self.cuisine2id = {}
+        print("Loading and stemming What's Cooking training dataset ...")
+        super(WhatsCookingStemmedDataset, self).__init__(None)
         self.ingredient_count = {} # used to calculate df in tf-idf
         self.porter = PorterStemmer()
         self.english_stopwords = set(stopwords.words('english'))
@@ -101,56 +101,35 @@ class WhatsCookingStemmedDataset:
                             self.ingredient2id[ingredient_stemmed] = \
                                 len(self.id2ingredient) - 1
                 self.cuisines.append(
-                    Cuisine(**{
-                        'id': item.get('id', None),
-                        'cuisine': item['cuisine'],
-                        'ingredients': ingredients_stemmed,
-                    })
+                    Cuisine(
+                        id=item['id'],
+                        cuisine=item['cuisine'],
+                        ingredients=ingredients_stemmed,
+                    )
                 )
-        print("Successfully loaded stemmed What's Cooking dataset!")
+        print("Successfully loaded stemmed What's Cooking training dataset!")
         print(
             "# of cuisines = %d; # of ingredients = %d" \
             % (len(self.id2cuisine), len(self.id2ingredient)),
         )
 
-    def __len__(self):
-        return len(self.cuisines)
-
-    def __getitem__(self, index):
-        return self.cuisines[index]
-
-    def save_to_text(self, sorting=True):
-        if sorting:
-            cuisine_output = self.id2cuisine.copy()
-            ingredient_output = self.id2ingredient.copy()
-            cuisine_output.sort()
-            ingredient_output.sort()
-        else:
-            cuisine_output = self.id2cuisine
-            ingredient_output = self.id2ingredient
-
-        with open('cuisine_labels.txt', 'w', encoding='utf-8') as f:
-            for cuisine in cuisine_output:
-                f.write(cuisine + '\n')
-        with open('ingredients_labels.txt', 'w', encoding='utf-8') as f:
-            for ingredient in ingredient_output:
-                f.write(ingredient + '\n')
-
     def load_test_file(self, file_path='dataset/test.json'):
+        print("Loading and stemming What's Cooking testing dataset ...")
         cuisines = []
         with open(file_path, encoding='utf-8', mode = 'r') as json_file:
-            for item in json.load(json_file):
+            for item in tqdm(json.load(json_file)):
                 ingredients_stemmed = [
                     self._stem_ingredient(ingredient)
                     for ingredient in item['ingredients']
                 ]
                 cuisines.append(
-                    Cuisine(**{
-                        'id': item.get('id', None),
-                        'cuisine': item.get('cuisine', None),
-                        'ingredients': ingredients_stemmed,
-                    })
+                    Cuisine(
+                        id=item['id'],
+                        cuisine=item.get('cuisine', None),
+                        ingredients=ingredients_stemmed,
+                    )
                 )
+        print("Successfully loaded stemmed What's Cooking testing dataset!")
         return cuisines
     
     def _stem_ingredient(self, ingredient):
@@ -181,14 +160,10 @@ class WhatsCookingStemmedDataset:
     
 # WhatsCookingStemmedSeparatedDataset splits  each ingredient into 
 # separate words
-class WhatsCookingStemmedSeparatedDataset:
+class WhatsCookingStemmedSeparatedDataset(WhatsCookingDataset):
     def __init__(self, file_path='dataset/train.json'):
-        print("Loading and stemming separated What's Cooking dataset ...")
-        self.cuisines = []
-        self.id2ingredient = []
-        self.ingredient2id = {}
-        self.id2cuisine = []
-        self.cuisine2id = {}
+        print("Loading and stemming separated What's Cooking training dataset ...")
+        super(WhatsCookingStemmedSeparatedDataset, self).__init__(None)
         self.porter = PorterStemmer()
         self.english_stopwords = set(stopwords.words('english'))
         with open(file_path, encoding='utf-8', mode = 'r') as json_file:
@@ -212,57 +187,36 @@ class WhatsCookingStemmedSeparatedDataset:
                             self.ingredient2id[token] = \
                                 len(self.id2ingredient) - 1
                 self.cuisines.append(
-                    Cuisine(**{
-                        'id': item.get('id', None),
-                        'cuisine': item['cuisine'],
-                        'ingredients': ingredients_stemmed,
-                    })
+                    Cuisine(
+                        id=item['id'],
+                        cuisine=item['cuisine'],
+                        ingredients=ingredients_stemmed,
+                    )
                 )
-        print("Successfully loaded stemmed and separated What's Cooking dataset!")
+        print("Successfully loaded stemmed and separated What's Cooking training dataset!")
         print(
             "# of cuisines = %d; # of ingredients = %d" \
             % (len(self.id2cuisine), len(self.id2ingredient)),
         )
 
-    def __len__(self):
-        return len(self.cuisines)
-
-    def __getitem__(self, index):
-        return self.cuisines[index]
-
-    def save_to_text(self, sorting=True):
-        if sorting:
-            cuisine_output = self.id2cuisine.copy()
-            ingredient_output = self.id2ingredient.copy()
-            cuisine_output.sort()
-            ingredient_output.sort()
-        else:
-            cuisine_output = self.id2cuisine
-            ingredient_output = self.id2ingredient
-
-        with open('cuisine_labels.txt', 'w', encoding='utf-8') as f:
-            for cuisine in cuisine_output:
-                f.write(cuisine + '\n')
-        with open('ingredients_labels.txt', 'w', encoding='utf-8') as f:
-            for ingredient in ingredient_output:
-                f.write(ingredient + '\n')
-
     def load_test_file(self, file_path='dataset/test.json'):
+        print("Loading and stemming separated What's Cooking testing dataset ...")
         cuisines = []
         with open(file_path, encoding='utf-8', mode = 'r') as json_file:
-            for item in json.load(json_file):
+            for item in tqdm(json.load(json_file)):
                 ingredients_stemmed = []
                 for ingredient in item['ingredients']:    
                     ingredients_stemmed += self._stem_and_separate_ingredient(
                         ingredient,
                     )
                 cuisines.append(
-                    Cuisine(**{
-                        'id': item.get('id', None),
-                        'cuisine': item.get('cuisine', None),
-                        'ingredients': ingredients_stemmed,
-                    })
+                    Cuisine(
+                        id=item['id'],
+                        cuisine=item.get('cuisine', None),
+                        ingredients=ingredients_stemmed,
+                    )
                 )
+        print("Successfully loaded stemmed and separated What's Cooking testing dataset!")
         return cuisines
     
     def _stem_and_separate_ingredient(self, ingredient):
@@ -287,11 +241,3 @@ class WhatsCookingStemmedSeparatedDataset:
         for punctuation in string.punctuation:
             token = token.replace(punctuation, '')
         return token
-
-
-if __name__ == "__main__":
-    dataset = WhatsCookingDataset()
-    print(dataset[0])
-    dataset.save_to_text()
-    test = dataset.load_test_file()
-    print(test[0])
